c9ea3536c7907e6b74166f3cd21706a0fbe63f75
Calvin Cheng <linchuan.cheng@gmail.com>
2015-07-16 16:02:55 +0800

    Write acceptance tests and make them pass

---
+++ tests/cucumber/features/landing-page/curate_a_landing_page.js
@@ -0,0 +1,18 @@
+module.exports = function() {
+
+  this.Given(/^I have created a landing page with the heading "([^"]*)"$/, function (heading, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    this.server.call('page/create', {path: '/', heading: heading}).then(callback);
+  });
+
+  this.When(/^a user navigates to the landing page$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    this.client.url(process.env.ROOT_URL).call(callback);
+  });
+
+  this.Then(/^they see the landing page "([^"]*)"$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+};
